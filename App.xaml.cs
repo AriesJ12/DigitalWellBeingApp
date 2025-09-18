7be35255483e2666fb6a1f31067f8e2e14c80cfd
AriesJ <ajtagle12@gmail.com>
2025-09-18 21:36:43 +0800

    made the tracking in the background(not tested)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using DigitalWellBeingApp.Services;
 
 namespace DigitalWellBeingApp;
 
@@ -9,5 +10,33 @@ namespace DigitalWellBeingApp;
 /// </summary>
 public partial class App : Application
 {
+    private readonly ActiveWindowTracker tracker;
+    private readonly ActiveWindowWatcher watcher;
+
+    public App()
+    {
+        tracker = new ActiveWindowTracker();
+        watcher = new ActiveWindowWatcher();
+    }
+    
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        watcher.OnActiveWindowChanged += (procName, pid, hwnd) =>
+        {
+            tracker.NotifyActiveProcess(procName);
+        };
+
+        watcher.Start();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+        watcher.Dispose();
+
+        // Cleanup logic here
+    }
 }
 
--- ViewModels/MainWindow.xaml.cs
+++ ViewModels/MainWindow.xaml.cs
@@ -6,28 +6,8 @@ namespace DigitalWellBeingApp.ViewModels
 {
     public partial class MainWindow : Window
     {
-        private readonly ActiveWindowTracker tracker;
-        private readonly ActiveWindowWatcher watcher;
-
         public MainWindow()
         {
-            InitializeComponent();
-
-            tracker = new ActiveWindowTracker();
-            watcher = new ActiveWindowWatcher();
-
-            watcher.OnActiveWindowChanged += (procName, pid, hwnd) =>
-            {
-                tracker.NotifyActiveProcess(procName);
-
-                // update the UI safely
-                Dispatcher.Invoke(() =>
-                {
-                    ActiveProcessText.Text = $"Active: {procName}";
-                });
-            };
-
-            watcher.Start();
         }
     }
 }
