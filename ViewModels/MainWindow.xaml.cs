5dc0b039a6afeb0f95efa9acfe6d16432b3a8428
AriesJ <ajtagle12@gmail.com>
2025-09-18 16:48:12 +0800

    created viewmodels

--- ViewModels/MainWindow.xaml.cs
+++ ViewModels/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using DigitalWellBeingApp.Services;
 
-namespace DigitalWellBeingApp  
+namespace DigitalWellBeingApp.ViewModels 
 {
     public partial class MainWindow : Window
     {
