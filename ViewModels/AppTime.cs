cca3c26297d1e7f3595026ada0fdab4f1272c43e
AriesJ <ajtagle12@gmail.com>
2025-09-19 17:24:33 +0800

    adding viewmodel

---
+++ ViewModels/AppTime.cs
@@ -0,0 +1,12 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace DigitalWellBeingApp.ViewModels
+{
+    public partial class AppTime : ObservableObject
+    {
+        private AppTime appTime;
+        
+    }
+}
