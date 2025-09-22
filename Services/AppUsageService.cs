61c16269c18d2ebbb536adb7808bea733696fb4d
AriesJ <ajtagle12@gmail.com>
2025-09-22 21:02:42 +0800

    add tracking saving for db

---
+++ Services/AppUsageService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using DigitalWellBeingApp.Data;
+using DigitalWellBeingApp.Models;
+
+namespace DigitalWellBeingApp.Services
+{
+    public class AppUsageService
+    {
+        // records app usage
+        public void TrackAppUsage(string processName, int durationSeconds)
+        {
+            using (var db = new AppUsageContext())
+            {
+                var today = DateTime.Today;
+
+                var existing = db.AppUsages
+                                 .FirstOrDefault(a => a.ProcessName == processName &&
+                                                      a.UsageDate == today);
+
+                if (existing != null)
+                {
+                    existing.DurationSeconds += durationSeconds;
+                }
+                else
+                {
+                    db.AppUsages.Add(new AppUsage
+                    {
+                        ProcessName = processName,
+                        UsageDate = today,
+                        DurationSeconds = durationSeconds
+                    });
+                }
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
