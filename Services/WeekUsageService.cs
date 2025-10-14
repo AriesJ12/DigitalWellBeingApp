e365043fb8f434e0a93ee1f7b467f835642b8300
AriesJ <ajtagle12@gmail.com>
2025-10-15 00:23:47 +0800

    added boiler plate

---
+++ Services/WeekUsageService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using DigitalWellBeingApp.Data;
+using DigitalWellBeingApp.Models;
+
+namespace DigitalWellBeingApp.Services
+{
+    public class WeekUsageService
+    {
+        
+    }
+}
