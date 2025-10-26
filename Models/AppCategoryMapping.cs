68aad5ad2e7844907cbbb9d81734b61c0031d382
AriesJ <ajtagle12@gmail.com>
2025-10-27 03:58:41 +0800

    added window title on the category mapping

--- Models/AppCategoryMapping.cs
+++ Models/AppCategoryMapping.cs
@@ -6,6 +6,8 @@ namespace DigitalWellBeingApp.Models
     {
         public int Id { get; set; }
         public string ProcessName { get; set; } = string.Empty;
+
+        public string WindowTitle { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
     }
 }
