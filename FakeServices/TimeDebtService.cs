d74e503ec7a4c78ebe1f12571a2dcbe4434d7a34
AriesJ <ajtagle12@gmail.com>
2025-11-04 11:44:37 +0800

    added fake service for category debt

--- FakeServices/TimeDebtService.cs
+++ FakeServices/TimeDebtService.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DigitalWellBeingApp.Models;
+using DigitalWellBeingApp.Models.Enums;
+
+/*
+FUNCTIONS:
+FUNC 1
+- MUST BE ABLE TO GET THE TIME DEBT BETWEEN 2 CATEGORIES
+
+FUNC 2
+- MUST BE ABLE TO CHECK WHEN THE APP IS BLOCKED OR NOT
+ */
 
 namespace DigitalWellBeingApp.FakeServices
 {
+   
     class TimeDebtService
     {
+        public CategoryDebt GetCategoryDebt() {
+            return new CategoryDebt
+            {
+                Id = 1,
+                SourceCategory = AppCategory.Entertainment,
+                TargetCategory = AppCategory.Productive,
+                Ratio = 2.0,
+                TriggerHours = 1.5
+            };
+        }
+
+        public bool IsCategoryBlocked(AppCategory category)
+        {
+            return true;
+        }
     }
 }
--- Models/CategoryDebt.cs
+++ Models/CategoryDebt.cs
@@ -1,18 +1,27 @@
 using DigitalWellBeingApp.Models.Enums;
 
-public class CategoryDebt
+namespace DigitalWellBeingApp.Models
 {
-    public int Id { get; set; }
+    public class CategoryDebt
+    {
+        public int Id { get; set; }
 
-    // e.g., "Entertainment" is the category being limited
-    public AppCategory SourceCategory { get; set; }
+        // e.g., "Entertainment" is the category being limited
+        public AppCategory SourceCategory { get; set; }
 
-    // e.g., "Productive" is the category you need to balance against
-    public AppCategory TargetCategory { get; set; }
+        // e.g., "Productive" is the category you need to balance against
+        public AppCategory TargetCategory { get; set; }
 
-    // e.g., 2 means you can do 2h Entertainment for every 1h Productive
-    public double Ratio { get; set; }
+        // e.g., 2 means you can do 2h Entertainment for every 1h Productive
+        public double Ratio { get; set; }
 
-    // e.g., after how many hours of SourceCategory before reminder triggers
-    public double TriggerHours { get; set; }
+        // e.g., after how many hours of SourceCategory before reminder triggers
+        public double TriggerHours { get; set; }
+
+
+        // Computed properties for UI display
+        public string SourceCategoryName => SourceCategory.ToString();
+        public string TargetCategoryName => TargetCategory.ToString();
+    }
+
 }
